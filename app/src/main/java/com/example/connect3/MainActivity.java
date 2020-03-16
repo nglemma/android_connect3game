7d5e5a20b50cbc3f705528daf666fe67e38a1043
nglemma <ngeleemmanuel@yahoo.com>
2020-03-16 13:57:55 +0100

    layout work2

--- app/src/main/java/com/example/connect3/MainActivity.java
+++ app/src/main/java/com/example/connect3/MainActivity.java
@@ -3,9 +3,18 @@ package com.example.connect3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity
 {
+    public void dropin (View view)
+    {
+       ImageView counter = (ImageView) view;
+       counter.setTranslationY(-1500);
+       counter.setImageResource(R.drawable.yellow);
+       counter.animate().translationYBy(1500).setDuration(300);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
